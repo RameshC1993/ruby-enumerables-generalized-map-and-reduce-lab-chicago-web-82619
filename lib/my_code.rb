2a85b35bf312f18377333dcab9d107bfc396621c
Ramesh Chuwan <chhetrirahul2015@gmail.com>
2019-08-20 20:37:27 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,7 +1,9 @@
-def map_to_negativize(source_array)
-  base  = "#{source_array}" * -1
-  puts base
-  yeld 
+def map_to_negativize(array, num)
+  result = "#{array} #{num}"
+  puts result
+  yield
   base
 end
 
+map_to_negativize{array, -1) do |innards|
+end
