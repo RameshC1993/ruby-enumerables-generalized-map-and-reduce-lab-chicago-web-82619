74f7ebea6a1f18f2facf4896b77ceb66f099a455
Ramesh Chuwan <chhetrirahul2015@gmail.com>
2019-08-20 22:13:13 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,3 +8,12 @@ def map(source_array)
   return new
 end
 
+def map(source_array)
+  new = []
+  i = 0
+  while i < source_array.length do
+    new << yield(source_array[i])
+    i += 1
+  end
+  return new
+end
